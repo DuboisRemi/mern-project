94b4387ae5ea8c4234d0abb0ed1416cb61e99186
Rémi <dbs.remi@gmail.com>
2021-11-29 17:15:08 +0100

    :
    Init

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern-project", {
+    UseNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
---
+++ controllers/user.controller.js
@@ -0,0 +1,104 @@
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+module.exports.getAllUsers = async (req, res) => {
+  const users = await UserModel.find().select("-password");
+  res.status(200).json(users);
+};
+
+module.exports.userInfo = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow: " + req.params.id);
+  try {
+    let user = await UserModel.findById(req.params.id).select("-password");
+    if (user) return res.status(200).json(user);
+    else return res.status(400).send("ID unknow: " + req.params.id);
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
+
+module.exports.updateUser = async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(400).send("ID unknow: " + id);
+  try {
+    let user = await UserModel.findOneAndUpdate(id, req.body, {
+      new: true,
+      upsert: true,
+    }).select("-password");
+    return res.status(200).send(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: err });
+  }
+};
+
+module.exports.deleteUser = async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(400).send("ID unknow: " + id);
+  try {
+    await UserModel.remove({ _id: id }).exec();
+    return res.status(200).json({ message: "Successfully deleted." });
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
+
+module.exports.follow = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow: " + req.params.id);
+  if (!ObjectID.isValid(req.body.idToFollow))
+    return res.status(400).send("ID unknow: " + req.body.idToFollow);
+  try {
+    // add to the following list
+    await UserModel.findByIdAndUpdate(
+      req.params.id,
+      { $addToSet: { following: req.body.idToFollow } },
+      { new: true },
+      (err, docs) => {
+        if (err) return res.status(400).json(docs);
+      }
+    ).clone();
+    // add to follower list
+    await UserModel.findByIdAndUpdate(
+      req.body.idToFollow,
+      { $addToSet: { followers: req.params.id } },
+      { new: true },
+      (err, docs) => {
+        if (err) return res.status(400).json(docs);
+      }
+    ).clone();
+    return res.status(201).json("Successfully followed");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ message: err });
+  }
+};
+
+module.exports.unfollow = async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) return res.status(400).send("ID unknow: " + id);
+  try {
+    // add to the followers list
+    await UserModel.findByIdAndUpdate(
+      id,
+      { following: req.body.idToFollow },
+      { new: true },
+      (err, docs) => {
+        if (err) return res.status(400).json(err);
+      }
+    ).clone();
+    //add to following list
+    await UserModel.findByIdAndUpdate(
+      req.body.idToFollow,
+      { followers: id },
+      { new: true, upsert: true },
+      (err, docs) => {
+        if (!err) return res.status(201).json(docs);
+        else return res.status(400).json(err);
+      }
+    ).clone();
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
+};
