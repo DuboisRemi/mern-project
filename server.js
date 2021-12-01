d96a86227a0d86337d3bbe2f3afb3261ea776aec
Rémi <dbs.remi@gmail.com>
2021-12-01 13:54:05 +0100

    implement CRUD post

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 const app = express();
 const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+//jwt
+app.use(checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 //routes
 app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
